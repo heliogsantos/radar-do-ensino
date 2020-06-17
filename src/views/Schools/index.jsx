e70301d89bca0dd9481a51b5703add38b1af741b
Helio Gonzaga <helio.santosjunior@outlook.com>
2020-06-17 13:53:32 -0300

    criei card demostrativo do bairro

--- src/views/Schools/index.jsx
+++ src/views/Schools/index.jsx
@@ -1,16 +1,28 @@
 import React from 'react'
 import { useParams } from "react-router-dom";
 
+import Check from '../../assets/check.svg'
+
 import './style.css'
 
 const Schools = () => {
+
   const { id } = useParams();
+
   return (
     <div className="schools-wraper">
-        <div className="card-school"></div>
-        <div className="card-school"></div>
-        <div className="card-school"></div>
-        <div className="card-school"></div>
+        <div className="schools-length">
+          <div className="box-left">
+            <div className="check">
+              <img src={Check} alt="Quantidade de Escolas"/>
+            </div>
+            <p>{id}</p>
+          </div>
+          <span className="length">20</span>
+        </div>
+        <div className="schools">
+          
+        </div>
     </div>
   );
 }
